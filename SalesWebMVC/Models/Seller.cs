323d28adb6df341b75f5e7b4603609757ae75172
jhulian11 <jhuliangamer@gmail.com>
2023-01-08 18:43:26 -0300

    Changing Seller Class SalesRecord Collection Initialization

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -32,13 +32,13 @@ namespace SalesWebMVC.Models
         [Display(Name = "Department")]
         public int DepartmentId { get; set; }
 
-        public ICollection<SalesRecord>? SalesRecords { get; set; }
+        public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
 
-        private readonly ICollection<SalesRecord> _salesRecords;
+        //private readonly ICollection<SalesRecord> _salesRecords;
 
         public Seller()
         {
-            _salesRecords = new List<SalesRecord>();
+            //_salesRecords = new List<SalesRecord>();
         }
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department) : this()
